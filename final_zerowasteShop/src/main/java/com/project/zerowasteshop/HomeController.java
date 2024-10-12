2712e018aac0b4fe29adbebfef6ef8cc7edbc0b5
Lee HyeonSu <leehs025@gmail.com>
2024-10-12 15:40:05 +0900

    기본 프로젝트 frame -패키지, view 폴더, 이미지 폴더

---
+++ final_zerowasteShop/src/main/java/com/project/zerowasteshop/HomeController.java
@@ -0,0 +1,20 @@
+package com.project.zerowasteshop;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import lombok.extern.slf4j.Slf4j;
+
+
+@Slf4j
+@Controller
+public class HomeController {
+	
+	@GetMapping({"/","/home"})//웰컴파일없이 제어가능(index.html무시)
+	public String home() {
+		log.info("/home");
+		return "home";//resources/templates폴더에서 찾는다
+	}
+	
+
+}
---
+++ final_zerowasteShop/src/test/java/com/project/zerowasteshop/FinalZerowasteShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.zerowasteshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalZerowasteShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
