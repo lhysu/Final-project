a2f2182009a897a66f73a74953f6efffb0c8cd2e
Kim Sohee <heeso819@gmail.com>
2024-10-15 16:17:52 +0900

    Add DeliveryController, DeliveryService, DeliveryMapper, sqlMapper_delivery.xml

---
+++ final_zerowasteShop/src/main/java/com/project/zerowasteshop/delivery/DeliveryController.java
@@ -0,0 +1,63 @@
+package com.project.zerowasteshop.delivery;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Controller
+public class DeliveryController {
+	
+	@Autowired
+	DeliveryService service;
+		
+	
+	@GetMapping("/delivery/d_selectOne")
+	public String d_selectOne(DeliveryVO vo, Model model) {
+		log.info("/delivery/d_selectOne");
+		log.info("vo : {}", vo);
+		
+		DeliveryVO vo2 = service.selectOne(vo);
+		log.info("vo2", vo2);
+		
+		model.addAttribute(vo2);
+		
+		return "delivery/d_selectOne";
+	}
+	
+	@GetMapping("/delivery/d_selectAll")
+	public String d_selectAll(Model model) {
+		log.info("/delivery/d_selectAll");
+		
+		List<DeliveryVO> list = service.selectAll();
+		log.info("list.size() : [}", list.size());
+		
+		model.addAttribute("list", list);
+		
+		return "delivery/d_selectAll";
+	}
+	
+	@GetMapping("/delivery/d_searchList")
+	public String d_searchList(Model model,
+			@RequestParam(defaultValue = "delivery_num") String searchKey,
+			@RequestParam(defaultValue = "2024") String searchWord) {
+		log.info("/delivery/d_searchList");
+		log.info("searchWord : {}", searchWord);
+		log.info("searchKey : {}", searchKey);
+		
+		List<DeliveryVO> list = service.searchList(searchKey, searchWord);
+		log.info("list.size() : {}", list.size());
+		
+		model.addAttribute("list", list);
+		
+		return "delivery/d_searchList";
+	}	
+	
+
+}
--- final_zerowasteShop/src/main/java/com/project/zerowasteshop/delivery/DeliveryVO.java
+++ final_zerowasteShop/src/main/java/com/project/zerowasteshop/delivery/DeliveryVO.java
@@ -6,7 +6,7 @@ public class DeliveryVO {
 
 	private int delivery_num;
 	private int pay_num;
-	private int product_num;
+	private int product_name;
 	private String member_id;
 	private int order_num;
 	private String tracking_num;
@@ -14,4 +14,98 @@ public class DeliveryVO {
 	private String delivery_status;
 	private Timestamp shipped_date;
 	private Timestamp delivery_date;
+	
+	@Override
+	public String toString() {
+		return "DeliveryVO [delivery_num=" + delivery_num + ", pay_num=" + pay_num + ", product_name=" + product_name
+				+ ", member_id=" + member_id + ", order_num=" + order_num + ", tracking_num=" + tracking_num
+				+ ", courier=" + courier + ", delivery_status=" + delivery_status + ", shipped_date=" + shipped_date
+				+ ", delivery_date=" + delivery_date + "]";
+	}
+
+	public int getDelivery_num() {
+		return delivery_num;
+	}
+
+	public void setDelivery_num(int delivery_num) {
+		this.delivery_num = delivery_num;
+	}
+
+	public int getPay_num() {
+		return pay_num;
+	}
+
+	public void setPay_num(int pay_num) {
+		this.pay_num = pay_num;
+	}
+
+	public int getProduct_name() {
+		return product_name;
+	}
+
+	public void setProduct_name(int product_name) {
+		this.product_name = product_name;
+	}
+
+	public String getMember_id() {
+		return member_id;
+	}
+
+	public void setMember_id(String member_id) {
+		this.member_id = member_id;
+	}
+
+	public int getOrder_num() {
+		return order_num;
+	}
+
+	public void setOrder_num(int order_num) {
+		this.order_num = order_num;
+	}
+
+	public String getTracking_num() {
+		return tracking_num;
+	}
+
+	public void setTracking_num(String tracking_num) {
+		this.tracking_num = tracking_num;
+	}
+
+	public String getCourier() {
+		return courier;
+	}
+
+	public void setCourier(String courier) {
+		this.courier = courier;
+	}
+
+	public String getDelivery_status() {
+		return delivery_status;
+	}
+
+	public void setDelivery_status(String delivery_status) {
+		this.delivery_status = delivery_status;
+	}
+
+	public Timestamp getShipped_date() {
+		return shipped_date;
+	}
+
+	public void setShipped_date(Timestamp shipped_date) {
+		this.shipped_date = shipped_date;
+	}
+
+	public Timestamp getDelivery_date() {
+		return delivery_date;
+	}
+
+	public void setDelivery_date(Timestamp delivery_date) {
+		this.delivery_date = delivery_date;
+	}
+	
+	
+	
+	
+	
+	
 }
