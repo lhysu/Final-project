f007e6b6b1d96df05228bcc241ea1a81c9962577
Dongwook Kim <dongwook258@naver.com>
2024-10-18 13:03:17 +0900

    Review selectAll 페이지 작성 및 연동

---
+++ final_zerowasteShop/src/main/java/com/project/zerowasteshop/review/model/ReviewVO.java
@@ -0,0 +1,23 @@
+package com.project.zerowasteshop.review.model;
+
+import java.sql.Timestamp;
+
+import org.json.JSONObject;
+import org.springframework.web.multipart.MultipartFile;
+
+import lombok.Data;
+
+@Data
+public class ReviewVO {
+	
+	private int review_num;
+    private String member_id;
+    private int product_num;
+    private String product_name;
+    private String content;
+    private double rating;
+    private String review_img;
+    private Timestamp createdDate;
+    private MultipartFile file;
+   
+}
