f0b46812420e97e3553759c14492d93e9662ddb7
Lee HyeonSu <leehs025@gmail.com>
2024-10-28 23:54:42 +0900

    CSS 수정, 이메일 인증 관련 수정 -로그인, 아이디찾기, 비밀번호찾기, 회원가입: 헤더, 푸터 추가 -기부가능 물품 조회 css 수정 -회원가입에 이메일 인증 추가 - 기부가능 물품 신청, 회원 생성, 회원 수정, 회원가입: 폼 유효성 검증 추가

--- final_zerowasteShop/src/main/java/com/project/zerowasteshop/member/mail/MailService.java
+++ final_zerowasteShop/src/main/java/com/project/zerowasteshop/member/mail/MailService.java
@@ -32,14 +32,14 @@ public class MailService implements MailServiceInter {
 		String msgg = "";
 		msgg += "<div style='margin:100px;'>";
 		msgg += "<h1> 안녕하세요</h1>";
-		msgg += "<h1> 제로웨이스트 쇼핑 플랫폼 zeroWaste 입니다.</h1>";
+		msgg += "<h1> 제로웨이스트 쇼핑 플랫폼 zero-C 입니다.</h1>";
 		msgg += "<br>";
-		msgg += "<p>아래 코드를 비밀번호 찾기 창으로 돌아가 입력해주세요<p>";
+		msgg += "<p>아래 코드를 쇼핑몰 창으로 돌아가 입력해주세요<p>";
 		msgg += "<br>";
 		msgg += "<p>저희 쇼핑몰을 이용해주셔서 감사합니다!<p>";
 		msgg += "<br>";
 		msgg += "<div align='center' style='border:1px solid black; font-family:verdana';>";
-		msgg += "<h3 style='color:blue;'>비밀번호 찾기 인증 코드입니다.</h3>";
+		msgg += "<h3 style='color:blue;'>이메일 인증 코드입니다.</h3>";
 		msgg += "<div style='font-size:130%'>";
 		msgg += "CODE : <strong>";
 		msgg += ePw + "</strong><div><br/> "; // 메일에 인증번호 넣기
