b55d251b7662d6ec2b508026612fcb0dc426382c
yang <tkwkdlq@gmail.com>
2024-11-05 15:06:28 +0900

    1. 사용자 ,관리자 프로젝트에서 서로 중복되는 내용 삭제 및 정리

--- final_zerowasteShop/src/main/java/com/project/zerowasteshop/coupon/CouponMapper.java
+++ final_zerowasteShop/src/main/java/com/project/zerowasteshop/coupon/CouponMapper.java
@@ -6,33 +6,12 @@ import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface CouponMapper {
-	public List<CouponVO> selectAll(String user_id);
 	
-	public int insertOK(CouponVO vo);
+	public List<CouponVO> selectAll(String user_id);
 
 	public int checkCouponCode(String couponCode);
 
 	public void registerCoupon(String couponCode,String member_id);
-
-	public List<CouponVO> selectAllPageBlock(int startRow, int pageBlock);
-
-	public int getTotalRows();
-
-	public void createCoupons(CouponVO coupon);
-
-	public List<CouponVO> searchListPageBlockId(String searchWord, int startRow, int pageBlock);
-
-	public List<CouponVO> searchListPageBlockUsed(String searchWord, int startRow, int pageBlock);
-
-	public int getSearchTotalRowsId(String searchWord);
-
-	public int getSearchTotalRowsUsed(String searchWord);
-
-	public CouponVO selectOne(CouponVO vo);
-
-	public int updateOK(CouponVO vo);
-
-	public int deleteCoupon(CouponVO vo);
 
 	public CouponVO getCouponInfo(String couponCode);
 
