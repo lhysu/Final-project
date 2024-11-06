c5cb614702b0cadd0875f484e36457e3c5bcede9
Lee HyeonSu <leehs025@gmail.com>
2024-11-06 14:49:07 +0900

    관리자 프로젝트 - 불필요한 기능 제거 (MemberController제거)

--- final_zerowasteShop_admin/src/main/java/com/project/zerowasteshop/member/MemberMapper.java
+++ final_zerowasteShop_admin/src/main/java/com/project/zerowasteshop/member/MemberMapper.java
@@ -4,29 +4,14 @@ import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
 
-import com.project.zerowasteshop.donateitem.DonateItemVO;
 
 @Mapper
 public interface MemberMapper {
-//
+
 	public MemberVO selectOne(String member_id);
-//
-//	public int insertOK(MemberVO vo);
-//
-//	public int updateOK(MemberVO vo);
-//
-//	public int deleteOK(MemberVO vo);
-//
-//	public MemberVO idCheck(String member_id);
-//
-//	public MemberVO login(MemberVO vo);
-//
-//	public MemberVO selectId(MemberVO vo);
-//
+
 	public void deductPoints(String member_id, int remainingPoints);
-//
-//	public int updatePW(String member_id, String name, String email, String temppw);
-//
+
 
 
 }
